97ec9d61a99381adf688e469d9fa91b056298337
test <yao_han613@163.com>
2021-01-29 01:53:07 +0800

    grpc-gateway

---
+++ resk/infra/base/etcd.go
@@ -0,0 +1,60 @@
+package base
+
+import (
+	"context"
+	"time"
+
+	"go.etcd.io/etcd/clientv3"
+	"resk.micro/infra"
+)
+
+var clt *clientv3.Client.Client
+
+type EtcdStarter struct {
+	starter infra.BaseStarter
+	cli *clientv3.Client
+}
+
+func (etcdclt *EtcdStarter) Init(ctx infra.StarterContext) {
+	client, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{""},
+		DialTimeout: 2 * time.Second,
+	})
+
+	if err != nil {
+		Logger().Fatal("etcd connect failed")
+	}
+	clt = client
+}
+
+func (etcdclt *EtcdStarter) Setup(ctx infra.StarterContext) {
+	ticker := time.NewTicker(time.Second * time.Duration(11))
+	kv := clientv3.NewKV(clt)
+	lease := clientv3.NewLease(clt)
+	var curleaseid clientv3.LeaseID = 0
+	for  {
+		if curleaseid == 0 {
+			grantResponse, err := lease.Grant(context.TODO(), 10)
+
+			key := grantResponse.ID
+		}
+	}
+}
+
+func (etcdclt *EtcdStarter) Start(ctx infra.StarterContext) {
+	panic("implement me")
+}
+
+func (etcdclt *EtcdStarter) StartBlocking() bool {
+	panic("implement me")
+}
+
+func (etcdclt *EtcdStarter) Stop(ctx infra.StarterContext) {
+	panic("implement me")
+}
+
+func GetEtcdClt(ctx context.Context) *clientv3.Client {
+
+	return clt
+}
+
